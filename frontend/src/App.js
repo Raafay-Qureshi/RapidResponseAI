01943d8981f132f49d52c1b0254472019b8c2089
Raafay-Qureshi <superraafay@gmail.com>
2025-11-07 02:20:38 -0500

    Initialized frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HAM - The Heimdall Alert Module</h1>
+        <p>Frontend Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
